936fcc0c01edbbe7c5da618d14acb78e463b0580
JanaliJayathilaka <jayathi-pe23018@stu.kln.ac.lk>
2026-04-17 13:58:48 +0530

    Create StaffMember.java
    lab04 q1 abstract parent created

---
+++ StaffMember.java
@@ -0,0 +1,26 @@
+abstract class StaffMember {
+    private String fullName;
+    private final String staffId; 
+    protected String department;
+    public StaffMember(String fullName, String staffId, String department) {
+        this.fullName = fullName;
+        this.staffId = staffId;
+        this.department = department;
+    }
+    public String getFullName() { 
+        return fullName; 
+    }
+    public String getStaffId() { 
+        return staffId; 
+    }
+    public String getDepartment() { 
+        return department; 
+    }
+    public final void displayBasicDetails() {
+        System.out.println("--- Staff Information ---");
+        System.out.println("ID: " + staffId);
+        System.out.println("Name: " + fullName);
+        System.out.println("Dept: " + department);
+    }
+    public abstract double calculateMonthlyPayment();
+}
